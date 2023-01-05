b64a106ede558b4124604da882e2835a734043f2
Kairos Jeong <kairosys@gmail.com>
2023-01-05 02:48:40 -0800

    flutter study2

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,55 @@ class MyApp extends StatelessWidget {
       // home: Text('안녕')
       // home: Icon(Icons.star)
       //   home: Image.asset('dog.jpg')
-      home: Center(
-        child: Container(width: 50, height: 50, color: Colors.blue),
+      // home: Center(
+      //   child: Container(width: 50, height: 50, color: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter 테스트를 위한 앱임'),
+
+      ),
+        // body: Container(),
+
+        body:Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children:[
+            Text('안녕'),
+            Icon(Icons.star),
+            Icon(Icons.star),
+            Icon(Icons.star),
+            Icon(Icons.star),
+        ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.phone),
+              label: 'Phone',
+              backgroundColor: Colors.green,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+              // backgroundColor: Colors.green,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+              // backgroundColor: Colors.purple,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+              // backgroundColor: Colors.pink,
+            ),
+          ]
+        ),
       )
     );
 
